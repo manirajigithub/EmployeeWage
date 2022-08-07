a77aeea63ab6b7a76d8cb26b24089f408eec44af
mani1 <mani1@NANI143>
2022-08-07 21:37:17 +0530

    Checking The Employee Attendance

---
+++ CheckEmployee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+
+    public class CheckEmployee
+    {
+        /// <summary>
+        /// Checks the employee present or not.
+        /// </summary>
+        public static void CheckEmployeePresentOrNot()
+        {
+            Random random = new Random();
+            int AttendanceVariable = random.Next(2);
+            int IsPresent = 1;
+
+            if (AttendanceVariable == IsPresent)
+
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+
+    }
+
+
+}
