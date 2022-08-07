4060bea7b58edf0a31aa08b8fce563143a1a63e9
mani1 <mani1@NANI143>
2022-08-07 21:34:47 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program .");
+            CheckEmployee.CheckEmployeePresentOrNot();
+        }
+    }
+}
