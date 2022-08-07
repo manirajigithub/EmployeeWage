160832e52ec91a17d10c3e9c2ca24a543bf09969
mani1 <mani1@NANI143>
2022-08-07 21:44:01 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWageProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program .");
+            PartTimeWage.DailyWage();
+        }
+    }
+}
