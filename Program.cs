78136eef08e70ba4dbef798d90f640977c7f5db1
mani1 <mani1@NANI143>
2022-08-08 15:51:25 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeeWageUC5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to employee wage program");
+            
+            monthlywage f = new monthlywage();
+            f.Monthwage();
+
+        }
+
+    }
+}
